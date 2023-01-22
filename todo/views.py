14565afb06c9662fb5d56af398241a7720dd8a3d
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-22 12:58:11 -0300

    primeira configuração da view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class IndexView(View):
+    template_name = 'todo/index.html'
+    http_method_names = ['get']
+
+    def get(self, request):
+        return render(request, self.template_name)
