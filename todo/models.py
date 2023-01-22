2d5091a6a6849c9d2a4d5983d58dd8bb17713115
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-22 12:57:18 -0300

    primeira configuração do model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ToDo(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 255)
+    comment = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.title
