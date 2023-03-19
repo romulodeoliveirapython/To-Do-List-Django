bb1e8b0a82e74d0e962ef294ea2826e78b4fa655
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-19 16:42:45 -0300

    criação do campo isComplete e id

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 
 class ToDo(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) # UUID, parecido com Java
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 255)
     comment = models.TextField(blank = True)
+    isComplete = models.BooleanField(default = False)
 
     def __str__(self):
         return self.title
