4657da4e023517a446a3a8e04204030760f31e33
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-19 17:16:47 -0300

    recuperando dados do model e passando os objetos para o template

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import ToDo
 
 
+"""
 class IndexView(View):
     template_name = 'todo/index.html'
     http_method_names = ['get']
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name) 
+"""
+
+class IndexView(View):
+    template_name = 'todo/todo-list.html'
+    http_method_names = ['get']
+    model = ToDo
+
+    def get(self, request):
+        # Recupera todos os objetos da model ToDo
+        todos = self.model.objects.all()
+
+        # Passa os objetos para o template usando o contexto
+        context = {'todos': todos}
+        return render(request, self.template_name, context)
