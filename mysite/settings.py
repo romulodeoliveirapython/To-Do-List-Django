a8a6a1f62a6608754faec29d6bcd4737fd00ced2
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-20 16:21:46 -0300

    Login/Logout redirect

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login/Logout redirect
+
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'todo:list' 
