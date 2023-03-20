3761089cf940b49f20c1d59b0029faf3d4978fe5
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-20 12:42:33 -0300

    mudança no nome da ListView e aplicação do decorator login_required

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
-from .models import ToDo
+from todo.models import ToDo
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
-class IndexView(ListView):
+@method_decorator(login_required, name='dispatch')
+class ToDoListView(ListView):
     template_name = 'todo/todo-list.html'
     model = ToDo
     context_object_name = 'todos'
 
 
+@method_decorator(login_required, name='dispatch')
 class TodoDetailView(DetailView):
     model = ToDo
     template_name = 'todo/todo-detail.html'
