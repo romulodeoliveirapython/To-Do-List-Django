dcb3395de336d0d33fed3cff8f00835e8c0e0fb7
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-20 12:42:06 -0300

    mudança do nome da ListView

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from todo.views import IndexView, TodoDetailView
+from todo.views import ToDoListView, TodoDetailView
 
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name = 'index'),
+    path('', ToDoListView.as_view(), name = 'list'),
     path('detail/<uuid:pk>/', TodoDetailView.as_view(), name = 'detail'),
 ]
