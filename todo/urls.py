0313dcb818c18bb3eaae17558dd15b46f7c8cec8
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-22 20:48:42 -0300

    criação da rota para update

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todo.views import ToDoListView, TodoDetailView
+from todo.views import ToDoListView, TodoDetailView, ToDoUpdateView
 
 
 app_name = 'todo'
@@ -7,4 +7,5 @@ app_name = 'todo'
 urlpatterns = [
     path('', ToDoListView.as_view(), name = 'list'),
     path('detail/<uuid:pk>/', TodoDetailView.as_view(), name = 'detail'),
+    path('update/<uuid:pk>/', ToDoUpdateView.as_view(), name = 'update')
 ]
