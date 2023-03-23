29ef38b6c33e76a5b7c3cd9291e906238854841d
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-22 21:30:35 -0300

    configuração da view para criação de tasks

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import View
 from todo.models import ToDo
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from todo.forms import ToDoForm
+
+
+@method_decorator(login_required, name='dispatch')
+class TodoCreateView(CreateView):
+    model = ToDo
+    form_class = ToDoForm
+    template_name = 'todo/todo-create.html'
+    success_url = reverse_lazy('todo:create')
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.user = self.request.user
+            return super().form_valid(form)
+        else:
+            return redirect('login')
 
 
 @method_decorator(login_required, name='dispatch')
@@ -30,4 +46,4 @@ class ToDoUpdateView(UpdateView):
     model: ToDo
     queryset: ToDo.objects.all()
     success_url = reverse_lazy('todo:list')
-    template_name = 'todo-update.html'
+    template_name = 'todo/todo-update.html'
