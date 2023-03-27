696629356d2426f2288408fa1bdc57fad75e3b89
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-27 20:13:04 -0300

    adição do context_object_name em algumas classes e mudança no success_url do CreateView

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ class TodoCreateView(CreateView):
     model = ToDo
     form_class = ToDoForm
     template_name = 'todo/todo-create.html'
-    success_url = reverse_lazy('todo:create')
+    success_url = reverse_lazy('todo:list')
 
     # Verifica se o user está autenticado. Caso não esteja, encaminha-o para a página de Login
     def form_valid(self, form):
@@ -21,14 +21,14 @@ class TodoCreateView(CreateView):
             form.instance.user = self.request.user
             return super().form_valid(form)
         else:
-            return redirect('accounts:login')
+            return redirect('login')
 
 
 @method_decorator(login_required, name='dispatch')
 class ToDoListView(ListView):
     template_name = 'todo/todo-list.html'
     model = ToDo
-    context_object_name = 'todos'
+    context_object_name = 'todo'
 
     # Lógica para que o usuário consiga ver somente os objetos relacionados ao seu user
     def get_queryset(self):
@@ -40,6 +40,7 @@ class TodoDetailView(DetailView):
     model = ToDo
     template_name = 'todo/todo-detail.html'
     slug_url_kwarg = 'slug'
+    context_object_name = 'todo'
 
 
 @method_decorator(login_required, name='dispatch')
@@ -48,9 +49,12 @@ class ToDoUpdateView(UpdateView):
     queryset: ToDo.objects.all()
     success_url = reverse_lazy('todo:list')
     template_name = 'todo/todo-update.html'
+    context_object_name = 'todo'
+
 
 @method_decorator(login_required, name='dispatch')
 class ToDoDeleteView(DeleteView):
     model = ToDo
     fields = '__all__'
     success_url = reverse_lazy('todo:list')
+    context_object_name = 'todo'
