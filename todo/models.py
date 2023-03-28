4966aaea5440d3aa2f15c004e39155202c0e6bea
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-28 19:59:31 -0300

    mudança do nome do model ToDo para Tasks e criação do model Profile

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 import uuid
 
 
-class ToDo(models.Model):
+class Tasks(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) # UUID, parecido com Java
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 255)
@@ -12,3 +12,7 @@ class ToDo(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
