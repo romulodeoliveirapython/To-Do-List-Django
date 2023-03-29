1d3a139b7bb3d4397bb310ce9aed5e1144bf79f2
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-29 17:08:14 -0300

    diminuição do tamanho do arquivo executeme

--- executeme.sh
+++ executeme.sh
@@ -6,18 +6,6 @@ sudo apt install python3.8-venv
 # Cria um ambiente virtual para o projeto
 python3 -m venv .venv
 
-# Ativa o ambiente virtual
-. .venv/bin/activate
-
-# Instala as dependências do projeto
-.venv/bin/pip install django
-
-# Executa as migrações do banco de dados
-.venv/bin/python manage.py migrate
-sleep 2
-.venv/bin/python manage.py makemigrations
-sleep 2
-
 # Cria um arquivo chamado "meuarquivo.txt" na pasta do projeto
 touch mysite/testing.py
 
@@ -32,9 +20,3 @@ database = {
         'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'db.sqlite3'),
     }
 }" > mysite/testing.py
-
-# Tempo
-sleep 20
-
-# Executa o servidor web do Django e abre o navegador no link 127.0.0.1:8000/
-.venv/bin/python manage.py runserver & xdg-open http://127.0.0.1:8000/
