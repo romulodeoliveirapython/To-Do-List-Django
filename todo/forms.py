7a3412144538914631687eb2e738661329911a1f
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-03-29 18:24:19 -0300

    exclusão do form do profile

--- todo/forms.py
+++ todo/forms.py
@@ -1,15 +1,8 @@
 from django import forms
 from todo.models import Tasks
-from django.contrib.auth.models import User
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
         exclude = ('user',)
-
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['password', 'username', 'first_name', 'last_name', 'email', 'date_joined']
